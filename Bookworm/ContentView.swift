d461980e2646b3636d521a5ff4424d700063e014
1Soyebo <soyeboibukun@gmail.com>
2022-06-22 15:16:13 +0100

    completed working with core data

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
    
     @AppStorage("notes") private var notes = ""
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
 
    var body: some View {
@@ -17,6 +18,19 @@ struct ContentView: View {
            VStack {
                List(students) { student in
                    Text(student.name ?? "Unknown")
+               }
+               
+               Button("Add") {
+                   let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                   let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                   let chosenFirstName = firstNames.randomElement()!
+                   let chosenLastName = lastNames.randomElement()!
+
+                   let student = Student(context: moc)
+                   student.id = UUID()
+                   student.name = "\(chosenFirstName) \(chosenLastName)"
+                   try? moc.save()
                }
            }
            .navigationTitle("Core Data")
