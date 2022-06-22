516d50a221f145f74a5ef99a119d22706cbf916d
1Soyebo <soyeboibukun@gmail.com>
2022-06-22 14:43:16 +0100

    first commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ibukunoluwa Soyebo on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Ibukunoluwa Soyebo on 22/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    @AppStorage("notes") private var notes = ""
+
+   var body: some View {
+       NavigationView {
+           TextEditor(text: $notes)
+               .navigationTitle("Notes")
+               .padding()
+       }
+   }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
